514a95ebc7f158fab65c6b0ef298ff7450c8a23a
陈德华 <hua@131.im>
2022-11-19 11:49:12 +0800

    * update

--- build.bat
+++ build.bat
@@ -50,62 +50,62 @@ docker rmi php:%php80%-fpm-alpine
 docker rmi php:%php81%-fpm-alpine
 docker rmi php:%php82%-fpm-alpine
 
-docker build -f 7.2/cli/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-7.2 .
-docker build -f 7.4/cli/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-7.4 .
-docker build -f 8.0/cli/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-8.0 .
-docker build -f 8.1/cli/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-8.1 .
-docker build -f 8.2/cli/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-8.2 .
-docker build -f 7.2/fpm/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-7.2 .
-docker build -f 7.4/fpm/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-7.4 .
-docker build -f 8.0/fpm/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-8.0 .
-docker build -f 8.1/fpm/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-8.1 .
-docker build -f 8.2/fpm/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-8.2 .
+docker build -f 7.2/cli/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-7.2 .
+docker build -f 7.4/cli/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-7.4 .
+docker build -f 8.0/cli/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-8.0 .
+docker build -f 8.1/cli/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-8.1 .
+docker build -f 8.2/cli/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-8.2 .
+docker build -f 7.2/fpm/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-7.2 .
+docker build -f 7.4/fpm/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-7.4 .
+docker build -f 8.0/fpm/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-8.0 .
+docker build -f 8.1/fpm/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-8.1 .
+docker build -f 8.2/fpm/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-8.2 .
 
-docker tag registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-7.2 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-%php72%
-docker tag registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-7.4 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-%php74%
-docker tag registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-8.0 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-%php80%
-docker tag registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-8.1 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-%php81%
-docker tag registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-8.2 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-%php82%
-docker tag registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-7.2 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-%php72%
-docker tag registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-7.4 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-%php74%
-docker tag registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-8.0 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-%php80%
-docker tag registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-8.1 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-%php81%
-docker tag registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-8.2 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-%php82%
+docker tag registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-7.2 registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-%php72%
+docker tag registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-7.4 registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-%php74%
+docker tag registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-8.0 registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-%php80%
+docker tag registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-8.1 registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-%php81%
+docker tag registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-8.2 registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-%php82%
+docker tag registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-7.2 registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-%php72%
+docker tag registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-7.4 registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-%php74%
+docker tag registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-8.0 registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-%php80%
+docker tag registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-8.1 registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-%php81%
+docker tag registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-8.2 registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-%php82%
 
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-7.2
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-7.4
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-8.0
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-8.1
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-8.2
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-7.2
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-7.4
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-8.0
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-8.1
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-8.2
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-7.2
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-7.4
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-8.0
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-8.1
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-8.2
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-7.2
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-7.4
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-8.0
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-8.1
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-8.2
 
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-%php72%
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-%php74%
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-%php80%
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-%php81%
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:cli-%php82%
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-%php72%
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-%php74%
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-%php80%
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-%php81%
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:fpm-%php82%
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-%php72%
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-%php74%
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-%php80%
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-%php81%
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:cli-%php82%
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-%php72%
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-%php74%
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-%php80%
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-%php81%
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:fpm-%php82%
 
 
 cd E:\service\docker-php\octane
-docker build -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:octane-8.1 .
-docker build -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:octane-%php81% .
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:octane-8.1
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:octane-%php81%
+docker build -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1 .
+docker build -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-%php81% .
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-%php81%
 
 cd E:\service\docker-php\nginx
-docker build -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:nginx-8.1 .
-docker build -t registry.cn-hangzhou.aliyuncs.com/okami/docker-php:nginx-%php81% .
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:nginx-8.1
-docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:nginx-%php81%
+docker build -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1 .
+docker build -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-%php81% .
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-%php81%
 
 
 
