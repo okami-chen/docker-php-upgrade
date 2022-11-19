11157b0f39d34305ccfa8629075dfcb3990dc7e9
陈德华 <hua@131.im>
2022-11-19 14:42:42 +0800

    * 删除镜像

--- build_nginx.bat
+++ build_nginx.bat
@@ -1,7 +1,9 @@
 docker build -f nginx/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1 .
 docker build -f nginx/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1.12 .
 docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1
+docker rmi registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1
 docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1.12
+docker rmi registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1.12
 
 @REM docker build -f nginx/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.0 .
 @REM docker build -f nginx/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.0.25 .
--- build_octane.bat
+++ build_octane.bat
@@ -1,7 +1,9 @@
 docker build -f octane/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1 .
 docker build -f octane/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1.12 .
 docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1
+docker rmi registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1
 docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1.12
+docker rmi registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1.12
 
 @REM docker build -f octane/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.0 .
 @REM docker build -f octane/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.0.25 .
