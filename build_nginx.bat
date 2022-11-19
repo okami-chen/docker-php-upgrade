81b735ba5c7ca87fa7f79d36dabb1a969641c8a8
陈德华 <hua@131.im>
2022-11-19 14:30:58 +0800

    * update

--- build_nginx.bat
+++ build_nginx.bat
@@ -1,4 +1,9 @@
-docker build -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1 .
-docker build -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1.12 .
+docker build -f nginx/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1 .
+docker build -f nginx/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1.12 .
 docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1
 docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.1.12
+
+@REM docker build -f nginx/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.0 .
+@REM docker build -f nginx/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.0.25 .
+@REM docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.0
+@REM docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:nginx-8.0.25
--- build_octane.bat
+++ build_octane.bat
@@ -1,4 +1,9 @@
-docker build -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1 .
-docker build -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1.12 .
+docker build -f octane/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1 .
+docker build -f octane/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1.12 .
 docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1
 docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.1.12
+
+@REM docker build -f octane/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.0 .
+@REM docker build -f octane/Dockerfile -t registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.0.25 .
+@REM docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.0
+@REM docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php-upgrade:octane-8.0.25
