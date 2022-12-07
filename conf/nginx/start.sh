5d65b6f05ddcff444f135cbe1ae593d511b79b77
陈德华 <chendehua@51xpx.com>
2022-12-07 15:12:39 +0800

    * upgrade php version

--- conf/nginx/start.sh
+++ conf/nginx/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 chmod +x /opt/*.sh
+
 if [ -f "/opt/app.sh" ];then
   /bin/bash /opt/app.sh
 fi
@@ -11,21 +12,30 @@ cat /run/nginx/nginx.conf > /etc/nginx/nginx.conf
 
 # For Laravel
 mkdir /home/wwwlogs && chown www-data:www-data /home/wwwlogs
-mkdir -p /var/www/html/storage/logs/
+mkdir -p /var/www/html/storage/logs/supervisor
 mkdir -p /var/www/html/storage/app/
 mkdir -p /var/www/html/storage/framework/
 chmod 777 -R /var/www/html/storage/
+
+if [ -d "/var/www/html/bootstrap/cache" ];then
+    chmod 777 -R /var/www/html/bootstrap/cache
+fi
 
 # For System
 echo "COLUMNS=500" >> /root/.bashrc
 echo 'alias ll="ls -la"' >> /root/.bashrc
 
 # For Supervisor
-if [ -d /var/www/html/supervisor.d/ ];then
+if [ -d "/var/www/html/supervisor.d" ];then
     cp /var/www/html/supervisor.d/*.ini /etc/supervisor.d
 fi
 
 # For PHP_FPM
 sed -i 's/no/yes/g' /usr/local/etc/php-fpm.d/zz-docker.conf
+
+if [ -f "/opt/run.sh" ];then
+  /bin/bash /opt/run.sh
+fi
+
 php-fpm
 nginx -g "daemon off;"
--- conf/octane/start.sh
+++ conf/octane/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 chmod +x /opt/*.sh
+
 if [ -f "/opt/app.sh" ];then
   /bin/bash /opt/app.sh
 fi
@@ -13,21 +14,28 @@ mkdir /home/wwwlogs
 chown www-data:www-data /home/wwwlogs
 
 # For Laravel
-mkdir -p /var/www/html/storage/logs/
+mkdir -p /var/www/html/storage/logs/supervisor
 mkdir -p /var/www/html/storage/app/
 mkdir -p /var/www/html/storage/framework/
 chmod 777 -R /var/www/html/storage/
+
+if [ -d "/var/www/html/bootstrap/cache" ];then
+    chmod 777 -R /var/www/html/bootstrap/cache
+fi
 
 # For System
 echo "COLUMNS=500" >> /root/.bashrc
 echo 'alias ll="ls -la"' >> /root/.bashrc
 
 # For Supervisor
-if [ -d /var/www/html/supervisor.d/ ];then
+if [ -d "/var/www/html/supervisor.d" ];then
     cp /var/www/html/supervisor.d/*.ini /etc/supervisor.d
 fi
 
 /usr/bin/supervisord -c /etc/supervisord.conf
 
+if [ -f "/opt/run.sh" ];then
+  /bin/bash /opt/run.sh
+fi
 
 nginx -g "daemon off;"
