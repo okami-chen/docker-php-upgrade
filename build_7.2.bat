a36bef59a347de88c7072f074e9e7f84c34f2a40
陈德华 <hua@131.im>
2023-01-12 15:01:02 +0800

    * support 7.2 && 7.3

---
+++ build_7.2.bat
@@ -0,0 +1,89 @@
+docker build -f Dockerfile-7.2-octane -t sync402/docker-php:octane-7.2.34 .
+docker push sync402/docker-php:octane-7.2.34
+docker tag sync402/docker-php:octane-7.2.34 sync402/docker-php:octane-7.2
+docker push sync402/docker-php:octane-7.2
+
+docker tag sync402/docker-php:octane-7.2.34 registry.cn-shanghai.aliyuncs.com/okami/docker-php:octane-7.2.34
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php:octane-7.2.34
+docker rmi registry.cn-shanghai.aliyuncs.com/okami/docker-php:octane-7.2.34
+
+docker tag sync402/docker-php:octane-7.2.34 registry.cn-shanghai.aliyuncs.com/okami/docker-php:octane-7.2
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php:octane-7.2
+docker rmi registry.cn-shanghai.aliyuncs.com/okami/docker-php:octane-7.2
+
+docker tag sync402/docker-php:octane-7.2.34 registry.cn-beijing.aliyuncs.com/okami/docker-php:octane-7.2.34
+docker push registry.cn-beijing.aliyuncs.com/okami/docker-php:octane-7.2.34
+docker rmi registry.cn-beijing.aliyuncs.com/okami/docker-php:octane-7.2.34
+
+docker tag sync402/docker-php:octane-7.2.34 registry.cn-beijing.aliyuncs.com/okami/docker-php:octane-7.2
+docker push registry.cn-beijing.aliyuncs.com/okami/docker-php:octane-7.2
+docker rmi registry.cn-beijing.aliyuncs.com/okami/docker-php:octane-7.2
+
+docker tag sync402/docker-php:octane-7.2.34 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:octane-7.2.34
+docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:octane-7.2.34
+docker rmi registry.cn-hangzhou.aliyuncs.com/okami/docker-php:octane-7.2.34
+
+docker tag sync402/docker-php:octane-7.2.34 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:octane-7.2
+docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:octane-7.2
+docker rmi registry.cn-hangzhou.aliyuncs.com/okami/docker-php:octane-7.2
+
+docker tag sync402/docker-php:octane-7.2.34 registry.cn-guangzhou.aliyuncs.com/okami/docker-php:octane-7.2.34
+docker push registry.cn-guangzhou.aliyuncs.com/okami/docker-php:octane-7.2.34
+docker rmi registry.cn-guangzhou.aliyuncs.com/okami/docker-php:octane-7.2.34
+
+docker tag sync402/docker-php:octane-7.2.34 registry.cn-guangzhou.aliyuncs.com/okami/docker-php:octane-7.2
+docker push registry.cn-guangzhou.aliyuncs.com/okami/docker-php:octane-7.2
+docker rmi registry.cn-guangzhou.aliyuncs.com/okami/docker-php:octane-7.2
+
+docker rmi sync402/docker-php:octane-7.2
+docker rmi sync402/docker-php:octane-7.2.34
+
+docker rmi php:7.2-cli-alpine
+docker rmi php:7.2-fpm-alpine
+docker rmi php:7.2.34-cli-alpine
+docker rmi php:7.2.34-fpm-alpine
+
+docker build -f Dockerfile-7.2-web -t sync402/docker-php:web-7.2.34 .
+docker push sync402/docker-php:web-7.2.34
+docker tag sync402/docker-php:web-7.2.34 sync402/docker-php:web-7.2
+docker push sync402/docker-php:web-7.2
+
+docker tag sync402/docker-php:web-7.2.34 registry.cn-shanghai.aliyuncs.com/okami/docker-php:web-7.2.34
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php:web-7.2.34
+docker rmi registry.cn-shanghai.aliyuncs.com/okami/docker-php:web-7.2.34
+
+docker tag sync402/docker-php:web-7.2.34 registry.cn-shanghai.aliyuncs.com/okami/docker-php:web-7.2
+docker push registry.cn-shanghai.aliyuncs.com/okami/docker-php:web-7.2
+docker rmi registry.cn-shanghai.aliyuncs.com/okami/docker-php:web-7.2
+
+docker tag sync402/docker-php:web-7.2.34 registry.cn-beijing.aliyuncs.com/okami/docker-php:web-7.2.34
+docker push registry.cn-beijing.aliyuncs.com/okami/docker-php:web-7.2.34
+docker rmi registry.cn-beijing.aliyuncs.com/okami/docker-php:web-7.2.34
+
+docker tag sync402/docker-php:web-7.2.34 registry.cn-beijing.aliyuncs.com/okami/docker-php:web-7.2
+docker push registry.cn-beijing.aliyuncs.com/okami/docker-php:web-7.2
+docker rmi registry.cn-beijing.aliyuncs.com/okami/docker-php:web-7.2
+
+docker tag sync402/docker-php:web-7.2.34 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:web-7.2.34
+docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:web-7.2.34
+docker rmi registry.cn-hangzhou.aliyuncs.com/okami/docker-php:web-7.2.34
+
+docker tag sync402/docker-php:web-7.2.34 registry.cn-hangzhou.aliyuncs.com/okami/docker-php:web-7.2
+docker push registry.cn-hangzhou.aliyuncs.com/okami/docker-php:web-7.2
+docker rmi registry.cn-hangzhou.aliyuncs.com/okami/docker-php:web-7.2
+
+docker tag sync402/docker-php:web-7.2.34 registry.cn-guangzhou.aliyuncs.com/okami/docker-php:web-7.2.34
+docker push registry.cn-guangzhou.aliyuncs.com/okami/docker-php:web-7.2.34
+docker rmi registry.cn-guangzhou.aliyuncs.com/okami/docker-php:web-7.2.34
+
+docker tag sync402/docker-php:web-7.2.34 registry.cn-guangzhou.aliyuncs.com/okami/docker-php:web-7.2
+docker push registry.cn-guangzhou.aliyuncs.com/okami/docker-php:web-7.2
+docker rmi registry.cn-guangzhou.aliyuncs.com/okami/docker-php:web-7.2
+
+docker rmi sync402/docker-php:web-7.2
+docker rmi sync402/docker-php:web-7.2.34
+
+docker rmi php:7.2-cli-alpine
+docker rmi php:7.2-fpm-alpine
+docker rmi php:7.2.34-cli-alpine
+docker rmi php:7.2.34-fpm-alpine
